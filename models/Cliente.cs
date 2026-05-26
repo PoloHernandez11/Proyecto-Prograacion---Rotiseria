3b9eeccb40a18a57e5b87c34cd7f01f59fcae844
PoloHernandez11 <hernandezpolo705@gmail.com>
2026-05-25 22:46:51 -0300

    models terminados

---
+++ models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Roticeria
+{
+    public class Cliente : Persona
+    {
+        private string Direccion { get; set; }
+        private int Telefono { get; set; }
+
+
+        public Cliente(string nombre, string apellido, int dni, string direccion, int telefono)
+            :base(nombre, apellido, dni)
+        { 
+            this.Telefono = telefono;
+            this.Direccion = direccion;
+        }
+
+
+    }
+}
---
+++ models/Retiro.cs
@@ -0,0 +1,25 @@
+﻿        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Roticeria
+{
+    public class Retiro: Despacho
+    {
+        private string TipoDespacho { get; set; }
+
+        public Retiro(TimeSpan hora, double costo, string tipoDespacho)
+            : base(hora, costo)
+        {
+            this.TipoDespacho = tipoDespacho;
+        }
+
+        public void EstadoPedido(Pedido pedido)
+        {
+            Console.WriteLine($"Retiro en local | Tipo: {TipoDespacho}");
+            pedido.MostrarProductos();
+        }
+    }
+}
